abf39a422cf66a0c7408ddc34df7c253cb310bea
Omkar Hadawale <omkarhadawale00@Gmail.com>
2026-09-03 23:08:13 +0530

    Yo

---
+++ Python/Assignment2SETA_1.py
@@ -0,0 +1,6 @@
+""" Write a python script which accepts 5 integer values and prints “DUPLICATES” if any of the values 
+entered are duplicates otherwise it prints “ALL UNIQUE”. Example: Let 5 integers are (32, 45, 90, 45, 
+6) then output “DUPLICATES” to be printed.  """
+
+
+
