ef14a7c9d4348699e5975cfea85da77538611591
Om000000 <112142981+Om000000@users.noreply.github.com>
2026-09-21 16:56:14 +0000

    Initiaal commit

---
+++ Assignment 4/Assignment4SETA1.java
@@ -0,0 +1,436 @@
+public class Assignment4SETA1{
+
+public static void main(String args[]){
+    if(args.length==0){
+        System.out.println("Enter at least one number");
+        return;
+    }
+    int integercounter=0;
+    for(String arg:args){
+        try{
+            Integer.parseInt(arg);
+            integercounter++;
+        }
+        catch(NumberFormatException e){
+            System.out.println("Not an integer in the args:"+arg);
+        }
+    }
+            System.out.println("The number of list intergers you enter are:"+integercounter);
+
+
+}
+
+
+}public class Assignment4SETA1{
+
+public static void main(String args[]){
+    if(args.length==0){
+        System.out.println("Enter at least one number");
+        return;
+    }
+    int integercounter=0;
+    for(String arg:args){
+        try{
+            Integer.parseInt(arg);
+            integercounter++;
+        }
+        catch(NumberFormatException e){
+            System.out.println("Not an integer in the args:"+arg);
+        }
+    }
+            System.out.println("The number of list intergers you enter are:"+integercounter);
+
+
+}
+
+
+public class Assignment4SETA1{
+
+public static void main(String args[]){
+    if(args.length==0){
+        System.out.println("Enter at least one number");
+        return;
+    }
+    int integercounter=0;
+    for(String arg:args){
+        try{
+            Integer.parseInt(arg);
+            integercounter++;
+        }
+        catch(NumberFormatException e){
+            System.out.println("Not an integer in the args:"+arg);
+        }
+    }
+            System.out.println("The number of list intergers you enter are:"+integercounter);
+
+
+}
+
+
+}
+pppublipublic class Assignment4SETA1{
+
+public static void main(String args[]){
+    if(args.length==0){
+        System.out.println("Enter at least one number");
+        return;
+    }
+    int integercounter=0;
+    for(String arg:args){
+        try{
+            Integer.parseInt(arg);
+            integercounter++;
+        }
+        catch(NumberFormatException e){
+            System.out.println("Not an integer in the args:"+arg);
+        }
+    }
+            System.out.println("The number of list intergers you enter are:"+integercounter);
+
+public class Assignment4SETA1{
+
+public static void main(String args[]){
+    if(args.length==0){
+        System.out.println("Enter at least one number");
+        return;
+    }
+    int integercounter=0;
+    for(String arg:args){
+        try{
+            Integer.parseInt(arg);
+            integercounter++;
+        }
+        catch(NumberFormatException e){
+            System.out.println("Not an integer in the args:"+arg);
+        }
+    }
+            System.out.println("The number of list intergers you enter are:"+integercounter);
+
+
+}
+
+
+}public class Assignment4SETA1{
+
+public static void main(String args[]){
+    if(args.length==0){
+        System.out.println("Enter at least one number");
+        return;
+    }
+    int integercounter=0;
+    for(String arg:args){
+        try{
+            Integer.parseInt(arg);
+            integercounter++;
+        }
+        catch(NumberFormatException e){
+            System.out.println("Not an integer in the args:"+arg);
+        }
+    }
+            System.out.println("The number of list intergers you enter are:"+integercounter);
+
+
+}
+
+
+public class Assignment4SETA1{
+
+public static void main(String args[]){
+    if(args.length==0){
+        System.out.println("Enter at least one number");
+        return;
+    }
+    int integercounter=0;
+    for(String arg:args){
+        try{
+            Integer.parseInt(arg);
+            integercounter++;
+        }
+        catch(NumberFormatException e){
+            System.out.println("Not an integer in the args:"+arg);
+        }
+    }
+            System.out.println("The number of list intergers you enter are:"+integercounter);
+
+
+}
+
+
+ublic class Assignment4SETA1{
+
+public static volic class Assignment4SETA1{
+lic class Assignment4SETA1{
+
+public static lic class Assignment4SETA1{
+
+lic class Assignment4SETA1{
+
+public staticlic class Assignment4SETA1{
+
+lic class Assignment4SETA1{
+
+public statilic class Assignment4SETA1{
+
+lic class Assignment4SETA1{
+
+public statlic class Assignment4SETA1{
+
+public statoid main(String args[]){
+    if(args.length==0){
+        System.out.println("Enter at least one number");
+        return;
+    }
+    int integercounter=0;
+    for(String arg:args){
+        try{
+            Integer.parseInt(arg);
+            integercounter++;
+        }
+        catch(NumberFormatException e){
+            System.out.println("Not an integer in the args:"+arg);
+        }
+    }
+            System.out.println("The number of list intergers you enter are:"+integercounter);
+
+
+}
+
+
+}lic class Assignment4SETA1{
+
+public statoid main(String args[]){
+    if(args.length==0){
+        System.out.println("Enter at least one number");
+        return;
+    }
+    int integercounter=0;
+    for(String arg:args){
+        try{
+            Integer.parseInt(arg);
+            integercounter++;
+        }
+        catch(NumberFormatException e){
+            System.out.println("Not an integer in the args:"+arg);
+        }
+    }
+            System.out.println("The number of list intergers you enter are:"+integercounter);
+
+
+}
+
+
+}lic class Assignment4SETA1{
+
+public statoid main(String args[]){
+    if(args.length==0){
+        System.out.println("Enter at least one number");
+        return;
+    }
+    int integercounter=0;
+    for(String arg:args){
+        try{
+            Integer.parseInt(arg);
+            integercounter++;
+        }
+        catch(NumberFormatException e){
+            System.out.println("Not an integer in the args:"+arg);
+        }
+    }
+            System.out.println("The number of list intergers you enter are:"+integercounter);
+
+
+}
+
+
+}lic class Assignment4SETA1{
+
+public statoid main(String args[]){
+    if(args.length==0){
+        System.out.println("Enter at least one number");
+        return;
+    }
+    int integercounter=0;
+    for(String arg:args){
+        try{
+            Integer.parseInt(arg);
+            integercounter++;
+        }
+        catch(NumberFormatException e){
+            System.out.println("Not an integer in the args:"+arg);
+        }
+    }
+            System.out.println("The number of list intergers you enter are:"+integercounter);
+
+
+}
+
+
+}lic class Assignment4SETA1{
+
+public statoid main(String args[]){
+    if(args.length==0){
+        System.out.println("Enter at least one number");
+        return;
+    }
+    int integercounter=0;
+    for(String arg:args){
+        try{
+            Integer.parseInt(arg);
+            integercounter++;
+        }
+        catch(NumberFormatException e){
+            System.out.println("Not an integer in the args:"+arg);
+        }
+    }
+            System.out.println("The number of list intergers you enter are:"+integercounter);
+
+
+}
+
+
+}lic class Assignment4SETA1{
+
+public statoid main(String args[]){
+    if(args.length==0){
+        System.out.println("Enter at least one number");
+        return;
+    }
+    int integercounter=0;
+    for(String arg:args){
+        try{
+            Integer.parseInt(arg);
+            integercounter++;
+        }
+        catch(NumberFormatException e){
+            System.out.println("Not an integer in the args:"+arg);
+        }
+    }
+            System.out.println("The number of list intergers you enter are:"+integercounter);
+
+
+}
+
+
+}lic class Assignment4SETA1{
+
+public statoid main(String args[]){
+    if(args.length==0){
+        System.out.println("Enter at least one number");
+        return;
+    }
+    int integercounter=0;
+    for(String arg:args){
+        try{
+            Integer.parseInt(arg);
+            integercounter++;
+        }
+        catch(NumberFormatException e){
+            System.out.println("Not an integer in the args:"+arg);
+        }
+    }
+            System.out.println("The number of list intergers you enter are:"+integercounter);
+
+
+}
+
+
+}lic class Assignment4SETA1{
+
+public statoid main(String args[]){
+    if(args.length==0){
+        System.out.println("Enter at least one number");
+        return;
+    }
+    int integercounter=0;
+    for(String arg:args){
+        try{
+            Integer.parseInt(arg);
+            integercounter++;
+        }
+        catch(NumberFormatException e){
+            System.out.println("Not an integer in the args:"+arg);
+        }
+    }
+            System.out.println("The number of list intergers you enter are:"+integercounter);
+
+
+}
+
+
+ic class Assignment4SETA1{
+
+public statoid main(String args[]){
+ic class Assignment4SETA1{
+
+public statoid main(ic class Assignment4SETA1{
+ic class Assignment4SETA1{
+public statoid main(String args[]){
+    if(argsic class Assignment4SETA1{
+public statoid main(ment4SETA1{
+public statoid main(ment4SETA1{
+public statoid mainment4SETA1{
+public statoid mainSment4SETA1{
+public statoid maiment4SETA1{
+public statoid maiStment4SETA1{
+public statoid mament4SETA1{
+public statoid maStrment4SETA1{
+public statoid mment4SETA1{
+public statoid mString args[]){    if(args.length==0){
+       ment4SETA1{
+public statoid mString args[]){    if(args.length==0){
+        System.ment4SETA1{
+public statoid mString args[]){    if(args.length==0){
+       ment4SETA1{
+public statoid mString args[]){    if(args.length==0){
+        Systemment4SETA1{
+public statoid mString args[]){    if(args.length==0){
+       ment4SETA1{
+public statoid mString args[]){    if(args.length==0){
+        Systement4SETA1{
+public statoid mString args[]){    if(ament4SETA1{
+public statoment4SETA1{
+public statod mString args[]){    if(arment4SETA1{
+public statment4SETA1{
+public statd mString args[]){    if(argment4SETA1{
+public stament4SETA1{
+public stad mString args[]ment4SETA1{
+bldbld mString args[]){    if(args.length==0){
+mbermber");
+        return;
+    }
+    int integercounter=0;
+    for(String arg:args){
+        try{
+            Integer.parseInt(arg);
+            integercounter++;
+     catch     catch(NumberFormatException e){
+     
+    }
+    }
+          System.out.printst intergers you enter are:"+integercounter);
+
+
+}
+st intergers you enter are:"+integercounter);
+
+
+}
+
+
+t intergers you enter are:"+integercounter);
+
+
+}t intergers you enter are:"+integercounter);
+
+t intergers you enter are:"+integercounter);
+
+}t intergers you enter are:"+integercounter);
+t intergers you enter are:"+integercounter);
+}
+t intergers you enter are:"+integercounter);
+ intergers you enter are:"+integercounter);
+}
+ntergers you enter are:"+integercounter);
+
+
+
